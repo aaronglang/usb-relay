d585e7f7ba6d2259a3565a6b9115cf75a05c53c8
Aaron Langley <aaronlangley@wsip-184-188-122-178.oc.oc.cox.net>
2018-04-16 15:04:20 -0700

    bug fixes

--- relay.js
+++ relay.js
@@ -170,7 +170,7 @@ class Relay {
 }
 const init = function(path) {
     if(typeof path === 'undefined') {
-        path = './node_modules/usb-relay/scripts/relaycmd';
+        throw new Error('PLEASE SPECIFY PATH TO EXECUTABLE example: \'./node_modules/usb-relay/scripts/relaycmd\'');
     }
     return new Relay(path);
 }
